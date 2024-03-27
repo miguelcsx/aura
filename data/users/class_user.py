e6d41307411cacd538d01007ebbad7a02b86fe1f
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-03-26 21:43:36 -0500

    inicio estructura (issue #2)

---
+++ data/users/class_user.py
@@ -0,0 +1,6 @@
+class User():
+    
+    # faltan definir atributos de este objeto y sus métodos 
+    def __init__(self, name: str, password: str) -> None:
+        self.name: str = name
+        self.password: str = password
