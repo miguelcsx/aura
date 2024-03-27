ee5ebd10f2dbfe31ea7bdef54f6acded7eb6d60c
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-03-27 13:31:37 -0500

    Ajuste (ordenar)

--- data/users/class_user.py
+++
@@ -1,6 +0,0 @@
-class User():
-    
-    # faltan definir atributos de este objeto y sus métodos 
-    def __init__(self, name: str, password: str) -> None:
-        self.name: str = name
-        self.password: str = password
