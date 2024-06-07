af79dc30d044d2a199741ca08da0bf39c3bf2b62
Miguel Cárdenas <78029302+miguelcsx@users.noreply.github.com>
2024-06-07 00:08:02 -0400

    feat: Creates new AI commands (#49)
    - Beta function to crete mind maps
    
    - Allows to ask, summarize and explain topics

--- app/server/commands/ai_command.py
+++ app/server/commands/ai_command.py
@@ -1,43 +1,57 @@
 # aura/app/server/commands/ai_command.py
 
-from enum import Enum
+import os
+from datetime import datetime
 import discord
+from app.utils.cmdline import run_npx_command
 from app.server.commands.discord_commands import Commands
-from app.sources.llms.google_ai import GoogleAI
-
-
-class Model(Enum):
-    GOOGLE_AI = "google_ai"
-    OPEN_AI = "open_ai"
+from app.sources.generative.text_model import TextModel
 
 
 class AICommand(Commands):
 
-    def __init__(self, model: Model = Model.GOOGLE_AI):
-        super().__init__()
-        self.model = model
+    def execute(self, sub_command: str, *args) -> discord.Embed:
+        ai = TextModel()
 
-    def execute(self, sub_command: str, prompt: str) -> discord.Embed:
-        ai = self.get_model()
-
-        if sub_command == "chat":
-            return self.chat(ai, prompt)
+        if sub_command == "ask":
+            return self.ask(ai, *args)
+        elif sub_command == "explain":
+            return self.explain(ai, *args)
+        elif sub_command == "summarize":
+            return self.summarize(ai, *args)
+        elif sub_command == "mindmap":
+            return self.generate_map(ai, *args)
         else:
             return self.get_help()["brief"]
 
-    def chat(self, ai: GoogleAI, prompt: str) -> discord.Embed:
-        response = ai.generate_text(prompt)
+    def ask(self, ai: TextModel, prompt: str) -> discord.Embed:
+        response = ai.ask(prompt)
         return self.create_embed(prompt, response, "")
 
-    def get_model(self):
-        if self.model == Model.GOOGLE_AI:
-            return GoogleAI()
-        else:
-            return None
+    def explain(self, ai: TextModel, topic: str) -> discord.Embed:
+        response = ai.explain(topic)
+        return self.create_embed(topic, response, "")
+
+    def summarize(self, ai: TextModel, text: str, type: str = "brief") -> discord.Embed:
+        response = ai.summarize(text, type)
+        return self.create_embed(text, response, "")
+
+    async def generate_map(self, ai: TextModel, text: str, user_id: str) -> None:
+        user_folder = os.path.join("data", "mindmaps", str(user_id))
+        # Create the user folder if it does not exist
+        os.makedirs(user_folder, exist_ok=True)
+        response = ai.create_mindmap(text)
+        filename = f"{user_id}_{datetime.utcnow().isoformat()}_mindmap.md"
+        mindmap_path = os.path.join(user_folder, filename)
+        with open(mindmap_path, "w", encoding="utf-8") as file:
+            file.write(response)
+
+        # Create the visual mind map
+        run_npx_command("markmap-cli", "--no-open", "--no-toolbar", "--offline", "-o", mindmap_path, f"{mindmap_path}.html")
 
     def get_help(self):
         return {
             "brief": "Commands to interact with the AI",
             "search": "Generate text from the AI",
-            "parameters": ["chat", "prompt"]
+            "parameters": ["ask", "prompt"]
         }
