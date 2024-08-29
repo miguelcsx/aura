72281e5545f91bd22c71198f9b30637d729b6552
Miguel Cárdenas <miguelecsx@gmail.com>
2024-08-29 18:55:06 -0500

    feat: #52 update models
    - re-design the models to adapt new workflow

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -25,8 +25,7 @@ router = APIRouter()
 
 @router.post("/users/", response_model=UserInDBBase)
 def create_user_endpoint(
-    user: UserCreate,
-    db: Session = Depends(get_db)
+    user: UserCreate, db: Session = Depends(get_db)
 ) -> UserInDBBase:
     return create_user(db, user)
 
@@ -41,18 +40,14 @@ def read_user(user_id: int, db: Session = Depends(get_db)) -> UserInDBBase:
 
 @router.get("/users/", response_model=list[UserInDBBase])
 def read_users(
-    skip: int = 0,
-    limit: int = 10,
-    db: Session = Depends(get_db)
+    skip: int = 0, limit: int = 10, db: Session = Depends(get_db)
 ) -> list[UserInDBBase]:
     return get_users(db, skip, limit)
 
 
 @router.put("/users/{user_id}", response_model=UserInDBBase)
 def update_user_endpoint(
-    user_id: int,
-    user: UserUpdate,
-    db: Session = Depends(get_db)
+    user_id: int, user: UserUpdate, db: Session = Depends(get_db)
 ) -> UserInDBBase:
     db_user = get_user(db, user_id)
     if db_user is None:
@@ -61,10 +56,7 @@ def update_user_endpoint(
 
 
 @router.delete("/users/{user_id}", response_model=UserInDBBase)
-def delete_user_endpoint(
-    user_id: int,
-    db: Session = Depends(get_db)
-) -> UserInDBBase:
+def delete_user_endpoint(user_id: int, db: Session = Depends(get_db)) -> UserInDBBase:
     user = delete_user(db, user_id)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import Session
 import uvicorn
 from app.api.endpoints import (
     user,
-    courses,
 )
 from app.database.session import (
     Base,
@@ -18,7 +17,7 @@ app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user.router)
-app.include_router(courses.router)
+
 
 def get_session():
     db = Session(bind=engine)
@@ -33,7 +32,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-def main() -> None:    
+def main() -> None:
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
